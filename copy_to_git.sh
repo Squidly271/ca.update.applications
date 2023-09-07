6197f1c5eb82a42e42de831d4df04a5062c8691e
Squidly271 <unraidsquid@gmail.com>
2023-09-07 14:56:38 -0400

    Update copy_to_git.sh

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -3,5 +3,5 @@
 mkdir -p "/tmp/GitHub/ca.update.applications/source/ca.update.applications/usr/local/emhttp/plugins/ca.update.applications/"
 
 cp /usr/local/emhttp/plugins/ca.update.applications/* /tmp/GitHub/ca.update.applications/source/ca.update.applications/usr/local/emhttp/plugins/ca.update.applications -R -v -p
-
+find . -maxdepth 9999 -noleaf -type f -name "._*" -exec rm -v "{}" \;
 
